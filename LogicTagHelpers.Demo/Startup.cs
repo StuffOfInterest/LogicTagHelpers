b0014bf78abe220d02146e7d360af7e7e482ee78
Delbert Matlock <delbert.matlock@gmail.com>
2021-04-24 14:48:28 -0400

    Convert to libman for client libraries.

--- LogicTagHelpers.Demo/Startup.cs
+++ LogicTagHelpers.Demo/Startup.cs
@@ -1,13 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LogicTagHelpers.Demo
 {
@@ -20,13 +15,11 @@ namespace LogicTagHelpers.Demo
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -35,8 +28,6 @@ namespace LogicTagHelpers.Demo
 			}
 			else
 			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 			app.UseHttpsRedirection();
