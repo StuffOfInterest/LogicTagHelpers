3ec05c42295c42e4a29b24829de04d6d6fc00f57
Delbert Matlock <delbert.matlock@gmail.com>
2021-04-25 10:06:27 -0400

    Fix unreachable code warning in cshtml file.

--- LogicTagHelpers.Tests/CreateClassTests.cs
+++ LogicTagHelpers.Tests/CreateClassTests.cs
@@ -34,5 +34,35 @@ namespace LogicTagHelpers.Tests
 			// Assert
 			Assert.IsNotNull(result);
 		}
+
+		[TestMethod]
+		public void CanCreateSwitchTagHelper()
+		{
+			// Act
+			var result = new SwitchTagHelper();
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void CanCreateCaseTagHelper()
+		{
+			// Act
+			var result = new CaseTagHelper();
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void CanCreateDefaultTagHelper()
+		{
+			// Act
+			var result = new DefaultTagHelper();
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
 	}
 }
