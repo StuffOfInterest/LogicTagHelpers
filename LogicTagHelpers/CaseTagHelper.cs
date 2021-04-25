d826ad60dae5454b0f9eaa64d6216f1011511aa1
Delbert Matlock <delbert.matlock@gmail.com>
2021-04-25 08:00:07 -0400

    Switch and case basic control.

---
+++ LogicTagHelpers/CaseTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace LogicTagHelpers
+{
+	[HtmlTargetElement("case", ParentTag = "switch")]
+	public class CaseTagHelper : TagHelper
+	{
+		public object Value { get; set; }
+
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			var switchContext = (SwitchContext)context.Items[SwitchContext.ContextKey];
+			if (Value.Equals(switchContext.Expression))
+			{
+				switchContext.MatchedContent = await output.GetChildContentAsync();
+				switchContext.HasMatch = true;
+			}
+
+			output.SuppressOutput();
+		}
+	}
+}
---
+++ LogicTagHelpers/SwitchContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace LogicTagHelpers
+{
+	internal class SwitchContext
+	{
+		public const string ContextKey = "Select-Context";
+
+		public object Expression { get; set; }
+		public bool HasMatch { get; set; }
+		public IHtmlContent MatchedContent { get; set; }
+		public IHtmlContent DefaultContent { get; set; }
+	}
+}
